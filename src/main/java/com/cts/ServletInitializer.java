86012b7fafb29d6b8ef9eb15f9908be3f96759f9
guptasoma <somag83@gmail.com>
2022-07-05 22:05:12 +0530

    new commit 4

--- src/main/java/com/cts/ServletInitializer.java
+++ src/main/java/com/cts/ServletInitializer.java
@@ -1,5 +1,13 @@
 package com.cts;
 
-public class ServletInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(MainApp.class);
+	}
 }
