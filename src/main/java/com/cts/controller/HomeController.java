3a3a1d6a5ad16c24168678e295a82f03c03fce12
guptasoma <somag83@gmail.com>
2022-07-05 21:30:33 +0530

    commit for war generation

--- src/main/java/com/cts/controller/HomeController.java
+++ src/main/java/com/cts/controller/HomeController.java
@@ -1,10 +1,11 @@
 package com.cts.controller;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController extends SpringBootServletInitializer{
 
 	@GetMapping("/")
 	public String showHome() {
