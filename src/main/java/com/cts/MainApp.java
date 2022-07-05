f59ae1e447a86428e92154f260c05d2b0ef29957
guptasoma <somag83@gmail.com>
2022-07-05 21:55:09 +0530

    new commit 2

--- src/main/java/com/cts/MainApp.java
+++ src/main/java/com/cts/MainApp.java
@@ -3,8 +3,10 @@ package com.cts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class MainApp extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class,args);
